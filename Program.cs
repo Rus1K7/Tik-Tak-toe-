050e0df05f28ac11a3677e0ed7aa5ac71cbeae6d
Ruslan <ruslan.khasanov.03@mail.ru>
2025-10-04 11:02:02 +0300

    Initial commit: Tic Tac Toe game

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using TicTacToe.Program;
+
+namespace TicTacToe.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameUI ui = new GameUI();
+            ui.Start();
+        }
+    }
+}
